bb034321c5176f14686db3bc3205e31656829674
Rubens <rubens.teixeira@outlook.com.br>
2020-07-26 15:22:36 +0000

    Remove old Stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7fea)agzt#$+opj(@%htikxso#%(b-6os8n56a9uw*o9&yjm75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['www.rubensmachado.info']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rubensmachado.pythonanywhere.com']
 
 # Application definition
 
